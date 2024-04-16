b1a833596ca4e36bafc32ccbadb14611eb93b3f8
albin-fabijan <163516249+albin-fabijan@users.noreply.github.com>
2024-04-16 15:15:40 +0200

    feature:day2

--- jour02/job06/script.js
+++ jour02/job06/script.js
@@ -1,8 +1,7 @@
 var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 var konamiIndex = 0;
 
-function checkKonami(event) 
-{
+function checkKonami(event) {
     if (event.key === konamiCode[konamiIndex]) 
     {
         konamiIndex++;
@@ -10,25 +9,42 @@ function checkKonami(event)
         {
             document.body.style.backgroundColor = "#2ea3f2";
             konamiIndex = 0;
-            var logo = document.createElement("img");
-    
-            // Spécification de la source de l'image (URL du logo)
-            logo.src = "logo_laplateforme_bleu3.png"; // Remplacez "url_du_logo.png" par l'URL de votre logo
-            
-            // Style pour positionner l'image au milieu de la page
-            logo.style.position = "fixed";
-            logo.style.top = "50%";
-            logo.style.left = "50%";
-            logo.style.transform = "translate(-50%, -50%)";
-            
-            // Ajout de l'image au corps (body) du document
-            document.body.appendChild(logo);
+            startAtomFall();
         }
-    } 
-    else 
-    {
-        konamiIndex = 0; 
+    } else {
+        konamiIndex = 0;
     }
 }
 
 document.addEventListener("keydown", checkKonami);
+
+function startAtomFall() 
+{
+    setInterval(createAtom, 1000);
+}
+
+function createAtom() {
+    var atom = document.createElement("img");
+    atom.src = "atome.png";
+    atom.style.position = "absolute";
+    atom.style.left = Math.random() * window.innerWidth + "px";
+    atom.style.top = -100 + "px";
+    atom.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+
+    document.body.appendChild(atom);
+
+    var speed = 2 + Math.random() * 3;
+    var interval = setInterval(
+        function () 
+    {
+        var currentTop = parseFloat(atom.style.top);
+        atom.style.top = currentTop + speed + "px";
+
+        if (currentTop > window.innerHeight) 
+        {
+            clearInterval(interval);
+            atom.remove();
+        }
+    }
+    , 50);
+}
